a73fd47da29d1ad31f0ad006b8d84493c3bb6494
rverma999 <rajanverma999@gmail.com>
2025-05-08 09:25:07 -0700

    upd

--- gemm_extended/run_analyzer.sh
+++ gemm_extended/run_analyzer.sh
@@ -2,5 +2,7 @@ source /tools/Xilinx/Vivado/2024.1/settings64.sh && source /opt/xilinx/xrt/setup
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/opt/xilinx/xrt/lib:/tools/Xilinx/Vitis/2024.1/aietools/lib/lnx64.o
 #cd gemm_extended
 make clean
-make analyze mult_X=4 mult_Y=2 mult_Z=2 DEBUG_PRINTS=1 
+make -p Work
+#make sim mult_X=2 mult_Y=2 mult_Z=1 DEBUG_PRINTS=1 
+make analyze mult_X=2 mult_Y=2 mult_Z=1 DEBUG_PRINTS=1 
 # vitis_analyzer ./Work/graph.aiecompile_summary
