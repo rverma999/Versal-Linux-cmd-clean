c7667a09ddeb6848a52ad4f5193503ce58c112c3
rverma999 <rajanverma999@gmail.com>
2025-05-11 17:28:55 -0700

    upd

--- gemm_extended_dma_1_burst_opr/run_analyzer.sh
+++ gemm_extended_dma_1_burst_opr/run_analyzer.sh
@@ -10,7 +10,7 @@ make clean
 #This is running 2 sims ,  first withoug dma and then simualted dma
 #make run_sim all VERBOSE=1  mult_X=1 mult_Y=1 mult_Z=1 DEBUG_PRINTS=1
 
-make TARGET=hw_emu all VERBOSE=1
+make TARGET=hw_emu all VERBOSE=1 | tee total.log
 
 #v++ -l -t hw_emu --platform /tools/Xilinx/Vitis/2024.1/base_platforms/xilinx_vck190_base_202410_1/xilinx_vck190_base_202410_1.xpfm pl_kernels/s2mm.xo pl_kernels/mm2s.xo libadf.a --save-temps -g --config system.cfg -o tutorial.xsa || (echo "task: [xsa] failed error code: $?"; exit 1) 
 # vitis_analyzer ./Work/graph.aiecompile_summary
