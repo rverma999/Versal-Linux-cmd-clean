3329e78d46d2a323119088c04fb50b0b3739c6b3
rverma999 <rajanverma999@gmail.com>
2025-05-17 16:02:10 -0700

    upd

--- gemm_extended_dma_1_burst_opr/run_analyzer.sh
+++ gemm_extended_dma_1_burst_opr/run_analyzer.sh
@@ -10,7 +10,15 @@ make clean
 #This is running 2 sims ,  first withoug dma and then simualted dma
 #make run_sim all VERBOSE=1  mult_X=1 mult_Y=1 mult_Z=1 DEBUG_PRINTS=1
 
-make TARGET=hw_emu all VERBOSE=1 | tee total.log
+mv total.log total.log.last ;make clean ;  make TARGET=hw_emu all VERBOSE=1 | tee total.log
+
+
+# if there is an port issue try doing folowing :
+#after all steps till package are done. 
+make run_emu TARGET=hw_emu
+
+
+#mv total.log total.log.last ;make clean ;  make TARGET=hw_emu run VERBOSE=1 | tee total.log
 
 # Linking
 #v++ -l -t hw_emu --platform /tools/Xilinx/Vitis/2024.1/base_platforms/xilinx_vck190_base_202410_1/xilinx_vck190_base_202410_1.xpfm pl_kernels/s2mm.xo pl_kernels/mm2s.xo libadf.a --save-temps -g --config system.cfg -o tutorial.xsa || (echo "task: [xsa] failed error code: $?"; exit 1) 
@@ -18,5 +26,8 @@ make TARGET=hw_emu all VERBOSE=1 | tee total.log
 # PAckage and hw_emu : 
 # make host package run_emu TARGET=hw_emu
 
+#in hw_emu see system diagram
+# vitis_analyzer ./v++.package_summary
 
+# To view Summary 
 # vitis_analyzer ./Work/graph.aiecompile_summary
